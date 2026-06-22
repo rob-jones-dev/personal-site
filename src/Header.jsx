9882e5bc59b448f2705f71b68646f86882d1fba4
Mr-G-Almighty <rob_jones_92@hotmail.co.uk>
2026-06-22 13:24:53 +0100

    Functionality and content of about page

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
             </header>
             <nav className="bg-sky-600 text-white">
                 <ul className="font-mono flex flex-row justify-center gap-10 py-4 text-lg">
-                    <li><a href='#'>// Home</a></li>
-                    <li><a href='#'>// About</a></li>
+                    <li><a href='/'>// Home</a></li>
+                    <li><a href='/about'>// About</a></li>
                     <li><a href='#'>// Projects</a></li>
                     <li><a href='#'>// Contact</a></li>
                     <li className='icon-link'><a href='https://github.com/rob-jones-dev' target='_blank' rel='noopener noreferrer'>// GitHub</a></li>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './Header.jsx'
+import About from './About.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Header />
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
