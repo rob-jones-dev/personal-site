306683ebdf14a420f66efdfe45e0afd30c4a92ba
Mr-G-Almighty <rob_jones_92@hotmail.co.uk>
2026-06-22 13:13:09 +0100

    Link styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
         Thanks for browsing my website, I hope you enjoy it. This website is in a constant state of development, so some things might be broken but I'm working on it. I also have let deployment of this website to the public be a priority, so some things might not be as polished as I would like them to be. I will be working on that as well.
       </p>
       <p>
-        A little about me, I'm currently in training to become a full stack developer. I've got some experience in a professional environment but mostly it was front-end stuff. Since then I've been using <a href="https://www.theodinproject.com/" target="_blank" rel="noopener noreferrer">The Odin Project</a> to learn more about back-end development and full stack development.
+        A little about me, I'm currently in training to become a full stack developer. I've got some experience in a professional environment but mostly it was front-end stuff. Since then I've been using <a className="text-blue-500 hover:italic hover:underline" href="https://www.theodinproject.com/" target="_blank" rel="noopener noreferrer">The Odin Project</a> to learn more about back-end development and full stack development.
       </p>
     </div>
   )
