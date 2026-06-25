3b1fe329c22ee7c92ce1086bdbab78ba8af31a69
Mr-G-Almighty <rob_jones_92@hotmail.co.uk>
2026-06-25 10:43:20 +0100

    Updated version number

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return(
         <footer className="bg-sky-600 flex justify-center items-center mt-auto">
             <p>
-                <span className="font-mono mr-5">v1.0.2b</span>
+                <span className="font-mono mr-5">v1.1.9b</span>
                 2026 | Exactly zero AI was used in the creation of this website.
             </p>
         </footer>
