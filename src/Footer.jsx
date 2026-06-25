f10697b2ae19d692fbddbeb04c23c0ad21c147ce
Mr-G-Almighty <rob_jones_92@hotmail.co.uk>
2026-06-25 10:39:46 +0100

    Footer positioning fix and styling updates

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return(
-        <footer className="bg-sky-600 flex justify-center items-center">
+        <footer className="bg-sky-600 flex justify-center items-center mt-auto">
             <p>
                 <span className="font-mono mr-5">v1.0.2b</span>
                 2026 | Exactly zero AI was used in the creation of this website.
