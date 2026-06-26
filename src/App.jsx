4fabf62bfb56d495e5a71b8ffcce8162bdc6edf6
Mr-G-Almighty <rob_jones_92@hotmail.co.uk>
2026-06-26 11:33:21 +0100

    Added todo list on homepage and added temporary content to contatct page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ export default function App() {
       <p>
         Thanks for browsing my website, I hope you enjoy it. This website is in a constant state of development, so some things might be broken but I'm working on it. I also have let deployment of this website to the public be a priority, so some things might not be as polished as I would like them to be. I will be working on that as well.
       </p>
+      <p><b>Current to-do:</b>
+        <ul className="ml-5 list-disc">
+          <li>Add content to about and projects</li>
+          <li>Improve styling</li>
+          <li>Improve accessibility standards</li>
+          <li>Tidy up animations</li>
+          <li>Figure out where to place CV</li>
+        </ul>
+      </p>
       <p>
         A little about me, I'm currently in training to become a full stack developer. I've got some experience in a professional environment but mostly it was front-end stuff. Since then I've been using <a className="text-blue-500 hover:italic hover:underline" href="https://www.theodinproject.com/" target="_blank" rel="noopener noreferrer">The Odin Project</a> to learn more about back-end development and full stack development.
       </p>
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,8 @@
 export default function Contact() {
     return (
-        <div className="flex body p-4">
-            <span className="text-2xl body-heading font-bold">Contact</span>
+        <div className="flex flex-col body p-4 mx-auto text-center">
+            <p><span className="text-2xl body-heading font-bold">Contact</span></p>
+            <p><b>Email:</b> rob_jones_92@hotmail.co.uk</p>
         </div>
     )
 }
