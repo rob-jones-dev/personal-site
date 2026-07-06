9307d780bcb7620c83ef34358aea7a43bad0c135
Mr-G-Almighty <rob_jones_92@hotmail.co.uk>
2026-07-06 14:59:38 +0100

    Updated projects content

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -10,8 +10,11 @@ export default function Projects() {
                 </div>
                 <div className="flex-1">
                     <span className="text-xl body-heading font-bold">Board Game Club</span>
+                    <p><a href="https://midlothianmeeples.games">midlothianmeeples.games</a></p>
                     <p>I regularly go to a board game night at a local community centre. This night is organized by members of the local community. People bring along board games and we put groups together, some organized beforehand, and play board games all evening!</p>
                     <p>This event is currently organized using a ticket-selling website, for estimations of attendance. I'm aiming to replace this system with a custom website with all the required functionality.</p>
+                    <p>This was the first website I made which interfaced with a database after learning about it through The Odin Project. I hosted this database on supabase, which I had never used before. I decided to code the entire project in React because it seemed to be the most efficient way to bring these things together.</p>
+                    <p>I also used environment variables to store the location of the database and keep the credentials secure.</p>
                 </div>
             </div>
         </div>
