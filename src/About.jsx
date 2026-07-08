e9938592ba65b819a1b1ed624479e6b9398d32ed
Mr-G-Almighty <rob_jones_92@hotmail.co.uk>
2026-07-08 15:02:31 +0100

    Updated content

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,22 @@
 export default function About() {
     return (
         <div className="flex body p-4">
-            <div className="sidebar basis-1/4 border-r-2 border-e-sky-600 text-right pr-2">
-                <a href="#"><b>2026</b> - The Odin Project</a>
+            <div className="sidebar flex flex-col basis-1/3 border-r-2 border-e-sky-600 text-right pr-2">
+                <a href="#devsite"><b>2026</b> - DevSite and Midlothian Meeples</a>
+                <a href="#top"><b>2026</b> - The Odin Project</a>
+                <a href="#family"><b>2020</b> - Family Caregiver</a>
             </div>
             <div className="font-Roboto p-4 mx-auto basis-3/4 about-container">
-                    <section>
+                    <section id="devsite">
+                        <span className="text-2xl body-heading font-bold">DevSite and Midlothian Meeples</span>
+                        <p>
+                            After finishing my training I decided to put the new things I'd learned into practice. This began with this site you see here! I made the decision that it was important to get this site live and available as soon as possible, so I made the first few pages and put some content on them and then put it live. Using Vercel for the first time and hosting the file on my GitHub made the process seamless and intuitive.
+                        </p>
+                        <p>
+                            After this site was up and running I shipped my first app! It's a small app called "Midlothian Meeples" and it's exclusively available on the web. This site is designed to allow people to sign up to a weekly board game event I attend, up to this point we were using a group chat to organize who was bringing what game and who was playing which game. This got confusing fast as people didn't know what games had space, and people couldn't easily find which games were being played in order to look them up to see if they'd enjoy them. I attempted to fix all of these problems in my app. It interfaces with a database I created on Supabase which stores all the information about who is attending, what games they are bringing, who will be hosting the game (sometimes someone brings a game and others play it) and who wants to play that game, as well as link to BoardGameGeek where people can read more about the game. This site is in constant development as more features are requested by the users and as I see opportunies to improve the user experience. These sites both went live in July 2026.
+                        </p>
+                    </section>
+                    <section id="top">
                         <span className="text-2xl body-heading font-bold">2026 - The Odin Project</span>
                         <p>
                             In 2026 I finished <a href="https://www.theodinproject.com">The Odin Project</a>, the Odin Project took the basic to intermediate skills I had with HTML and CSS and built on them massively. If you've not heard of The Odin Project (TOP) before it's a pretty standard online course in learning various web development skills but very in depth. During this course I learned the following skills
@@ -20,6 +31,18 @@ export default function About() {
                             <li><b>Database Management</b> - We used PostgreSQL but we learned many of the SQL basics and some moderate concepts such that I can interact with a Database in a web app</li>
                             <li><b>Node.js</b> - We learned how to create and render websites on the server using Express, using routes, controllers and views. We learned how to secure a website with Passport.js we used Prisma to interact with our database. We also learned how to create and use our own APIs</li>
                         </ul>
+                    </section>
+                    <section id="family">
+                        <span className="text-2xl body-heading font-bold">Family Caregiver</span>
+                        <p>
+                            A few years ago my Mother was diagnosed with breast cancer. My father cannot drive and as a result it fell to me to look after my parents. I am very happy to say that my Mother has made a full recovery after 2 surgeries followed by chemotherapy and radiotherapy. I was just starting to get back into the world of WebDev.
+                        </p>
+                        <p>
+                            When my Dad had a heart attack. I had just moved to Edinburgh and my family is based in Hemel Hempstead, just outside London. My partner, who I'd move to Edinburgh to live with, and I went down to Hemel where, along with my brother, we looked after my Mother and went to visit my Father in the hospital. I am also very happy to say that my Father has made a recovery.
+                        </p>
+                        <p>
+                            I started learning Full Stack Development before this took place, and these events really put a damper on my learning. It was very difficult to resume my life after this and I still struggle with it.
+                        </p>
                     </section>
 
                 <p>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -9,12 +9,13 @@ export default function Projects() {
                     <p>The first project is one I've been very passionate about for years. One of my hobbies in my private time is playing video games, and I feel that video games reviews and judgement is often either boiled down to either 'good' or 'bad', else it's not given a judgement at all, instead other similar games are pointed to and a vague gesture is made.</p>
                 </div>
                 <div className="flex-1">
-                    <span className="text-xl body-heading font-bold">Board Game Club</span>
-                    <p><a href="https://midlothianmeeples.games">midlothianmeeples.games</a></p>
+                    <span className="text-xl body-heading font-bold">Midlothian Meeples</span>
+                    <p><a class="underline text-sky-600 after:content-['_↗']" target="_blank" href="https://midlothianmeeples.games">midlothianmeeples.games</a></p>
                     <p>I regularly go to a board game night at a local community centre. This night is organized by members of the local community. People bring along board games and we put groups together, some organized beforehand, and play board games all evening!</p>
                     <p>This event is currently organized using a ticket-selling website, for estimations of attendance. I'm aiming to replace this system with a custom website with all the required functionality.</p>
                     <p>This was the first website I made which interfaced with a database after learning about it through The Odin Project. I hosted this database on supabase, which I had never used before. I decided to code the entire project in React because it seemed to be the most efficient way to bring these things together.</p>
                     <p>I also used environment variables to store the location of the database and keep the credentials secure.</p>
+                    <p>I decided to host this on Vercel as it seems to be a popular hosting solution and I thought the experience with it would prove useful</p>
                 </div>
             </div>
         </div>
